de8af07c9b4781c308eed81d6ec85d90bf4dbe77
Aryan Jha <aryanjha@gmail.com>
2021-07-17 19:21:24 -0400

    added body parser and connected to mongodb backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
-const express = require('express');
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3001;
+    const PORT = process.env.PORT || 3001;
 
-const app = express();
+    const app = express();
 
+    app.use(
+        bodyParser.urlencoded({
+            extended: false
+        })
+    )
+    app.use(bodyParser.json());
 
-app.get("/api", (req, res) => {
-    res.json({ message: "my app" });
-  });
+    const database = require("./config/keys").mongoURI;
+    mongoose.connect(
+        database,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        },
+    )
+    .then(() => console.log("Connected to database successfully"))
+    .catch(err => console.log(err));
+ 
 
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+    app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+    });
