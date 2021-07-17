0038fe9c7aa13cceb8417052ea024597382fe71e
Aryan Jha <aryanjha@gmail.com>
2021-07-17 19:32:27 -0400

    added firebase config to front end

--- frontend/pickup-mobile/App.js
+++ frontend/pickup-mobile/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Firebase from './config/firebase';
 
 export default function App() {
   return (
